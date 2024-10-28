bced4baeda3db65e40127467859860257f35fb89
Maksymilian Piotrowski <maksymilian_piotrowski@wp.pl>
2024-10-28 18:03:02 +0100

    Fixed random dates, fixed incompatible registration numbers

--- generate_db/generate_db.py
+++ generate_db/generate_db.py
@@ -14,6 +14,20 @@ NUM_RESERVATIONS = 300000
 NUM_PAYMENTS = 300000
 NUM_CHARGES = 300000
 
+def generate_random_datetime(max_years_ago=5):
+    # Wygeneruj losową datę sprzed maksymalnie 5 lat
+    random_days_ago = random.randint(0, max_years_ago * 365)
+    base_date = datetime.now() - timedelta(days=random_days_ago)
+    random_time = timedelta(
+        hours=random.randint(0, 23),
+        minutes=random.randint(0, 59),
+        seconds=random.randint(0, 59),
+        microseconds=random.randint(0, 999999)
+    )
+    
+    random_datetime = base_date - random_time
+    return random_datetime
+
 def generate_parking_data(num_parking=NUM_PARKING):
     data = []
     for _ in range(num_parking):
@@ -61,13 +75,14 @@ def generate_parking_user_data(num_users=NUM_USERS, num_parking=NUM_PARKING):
         })
     return data
 
-
 def generate_client_car_data(num_cars=NUM_CARS, num_users=NUM_USERS):
     data = []
     for _ in range(num_cars):
+
         data.append({
             'id': _ + 1,
-            'addition_time': datetime.now().isoformat(),
+            # Wygeneruj datę dodania samochodu sprzed maksymalnie 5 lat
+            'addition_time': generate_random_datetime(max_years_ago=5).isoformat(), 
             'client_id': random.randint(1, num_users),
             'active': random.choice(['Y', 'N']),
             'registration_number': fake.unique.license_plate(),
@@ -109,22 +124,56 @@ def generate_parking_spot_data(num_spots=NUM_SPOTS, num_parking=NUM_PARKING):
     return data
 
 
-def generate_reservation_data(num_reservations=NUM_RESERVATIONS, num_spots=NUM_SPOTS,
-                              num_users=NUM_USERS):
+def generate_reservation_data(num_reservations=NUM_RESERVATIONS, num_spots=NUM_SPOTS, client_cars=None):
+    if client_cars is None:
+        raise ValueError("Client cars data must be provided.")
+
+    # Słownik z samochodami po id klienta
+    cars_by_user = {}
+    for car in client_cars:
+        user_id = car['client_id']
+        if user_id not in cars_by_user:
+            cars_by_user[user_id] = []
+        cars_by_user[user_id].append(car)
+    
     data = []
     for _ in range(num_reservations):
-        start_date = datetime.now()
-        end_date = start_date + timedelta(days=1)
+        # Wylosuj użytkownika, weź listę przypisanych do niego samochodów w celu zdobycia numeru rejestracyjnego
+        # Czas rezerwacji start_date musi być większy od addtion_time samochodu
+        user_id = random.choice(list(cars_by_user.keys()))
+        user_cars = cars_by_user[user_id]
+
+        selected_car = random.choice(user_cars)
+        registration_number = selected_car['registration_number']
+        
+        addition_time = datetime.fromisoformat(selected_car['addition_time'])
+        min_start_date = addition_time
+        start_date = min_start_date + timedelta(
+            days=random.randint(0, 31),
+            hours=random.randint(0, 23),
+            minutes=random.randint(0, 59),
+            seconds=random.randint(0, 59),
+            microseconds=random.randint(0, 999999)
+        )
+        end_date = start_date + timedelta(
+            days=random.randint(0, 7),
+            hours=random.randint(0, 23),
+            minutes=random.randint(0, 59),
+            seconds=random.randint(0, 59),
+            microseconds=random.randint(0, 999999)
+        )
+        
         data.append({
             'id': _ + 1,
             'parking_spot_id': random.randint(1, num_spots),
-            'user_id': random.randint(1, num_users),
+            'user_id': user_id,
             'start_date': start_date.isoformat(),
             'end_date': end_date.isoformat(),
             'amount': round(random.uniform(10.0, 50.0), 2),
             'active': random.choice(['Y', 'N']),
-            'registration_number': fake.unique.license_plate()
+            'registration_number': registration_number
         })
+    
     return data
 
 
@@ -176,7 +225,7 @@ if __name__ == "__main__":
     parking_user_data = generate_parking_user_data()
     client_car_data = generate_client_car_data()
     parking_spot_data = generate_parking_spot_data()
-    reservation_data = generate_reservation_data()
+    reservation_data = generate_reservation_data(client_cars=client_car_data)
     payment_data = generate_payment_data()
     stripe_charge_data = generate_stripe_charge_data()
 

b7b4bc594f7898215364f4a3c0abb4e18d119f96
byteingWolf <wilczek4fun@gmail.com>
2024-11-04 23:56:38 +0100

    Adding short script to remove all tables.

---
+++ WARNING_extermination.sql
@@ -0,0 +1,10 @@
+BEGIN
+
+  --Bye Tables!
+  FOR i IN (SELECT ut.table_name
+              FROM USER_TABLES ut) LOOP
+    EXECUTE IMMEDIATE 'drop table '|| i.table_name ||' CASCADE CONSTRAINTS ';
+  END LOOP;
+
+END;
+

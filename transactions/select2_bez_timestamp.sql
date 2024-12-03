b621ae79a53d652a1ae97c031e0310b261a6accd
byteingWolf <wilczek4fun@gmail.com>
2024-12-03 16:18:26 +0100

    a

---
+++ transactions/select2_bez_timestamp.sql
@@ -0,0 +1,33 @@
+SELECT cc.brand,
+       cc.color,
+       COUNT(r.id) AS total_parking_count,
+       (SELECT COUNT(*)
+        FROM Reservation r_sub
+                 JOIN ClientCar cc_sub
+                      ON cc_sub.registration_number = r_sub.registration_number
+        WHERE r_sub.user_id = r.user_id
+          AND cc_sub.brand = cc.brand
+          AND r_sub.start_date BETWEEN :start_date  AND :end_date
+           )       AS user_specific_brand_count,
+       (SELECT COUNT(*)
+        FROM Reservation r_sub
+                 JOIN ClientCar cc_sub
+                      ON cc_sub.registration_number = r_sub.registration_number
+        WHERE cc_sub.brand = cc.brand
+          AND r_sub.start_date BETWEEN :start_date  AND :end_date
+           )       AS overall_brand_parking_count,
+       (SELECT COUNT(*)
+        FROM Reservation r_sub
+        WHERE r_sub.parking_spot_id = r.parking_spot_id
+          AND r_sub.start_date BETWEEN :start_date  AND :end_date
+           )       AS parking_spot_total
+FROM ClientCar cc
+         JOIN
+     Reservation r ON cc.registration_number = r.registration_number
+         JOIN
+     ParkingSpot ps ON r.parking_spot_id = ps.id
+         JOIN
+     Parking p ON ps.parking_id = p.id
+WHERE r.start_date BETWEEN :start_date  AND :end_date
+GROUP BY cc.brand, cc.color, r.user_id, r.parking_spot_id
+ORDER BY total_parking_count DESC
